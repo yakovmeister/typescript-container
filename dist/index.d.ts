811e148e29248c73cf4de43d0ff248a3800a00f0
yakovmeister <electro7bug@gmail.com>
2018-05-27 19:44:00 +0800

    adding d.ts: i'm not aware about this thing.

---
+++ dist/index.d.ts
@@ -0,0 +1,13 @@
+import Container from './src/Container'
+import { ContextualBindingBuilder } from './src/ContextualBindingBuilder'
+import { Inject } from './src/Container'
+import { end } from './src/utils/end'
+import { isInstantiable } from './src/utils/isInstantiable'
+
+export {
+  Container,
+  ContextualBindingBuilder,
+  Inject,
+  end,
+  isInstantiable
+}
